8b9af82da20e24fe98e45ac8a676eefa7ef1ad50
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2019-04-22 17:01:46 -0300

    First Step

---
+++ app/src/main/java/com/example/whatmoviesnow/scenes/dramaList/DramaListFragment.kt
@@ -0,0 +1,24 @@
+package com.example.whatmoviesnow.scenes.dramaList
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.whatmoviesnow.R
+
+class DramaListFragment: Fragment() {
+    
+    companion object {
+        fun newInstance(): DramaListFragment { return DramaListFragment() }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_drama_movies_list, container, false)
+    }
+}
